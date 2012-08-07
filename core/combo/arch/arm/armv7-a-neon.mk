ff9eecfaaeec10ad1717228ac07a70247e8cfa4a
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-08-06 23:15:56 -0700

    Add standard method for enabling mfpu and mcpu code optimization
    Conflicts:
    	core/combo/arch/arm/armv7-a-neon.mk

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -1,25 +1,6 @@
 # Configuration for Linux on ARM.
 # Generating binaries for the ARMv7-a architecture and higher with NEON
 #
-ARCH_ARM_HAVE_ARMV7A            := true
-ARCH_ARM_HAVE_TLS_REGISTER      := true
-ARCH_ARM_HAVE_VFP               := true
-ARCH_ARM_HAVE_VFP_D32           := true
-ARCH_ARM_HAVE_NEON              := true
+TARGET_ARCH_VARIANT_FPU := neon
+include $(BUILD_COMBOS)/arch/$(TARGET_ARCH)/armv7-a.mk
 
-ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a15)
-	arch_variant_cflags := -mcpu=cortex-a15
-else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7
-else
-	arch_variant_cflags := -march=armv7-a
-endif
-endif
-
-arch_variant_cflags += \
-    -mfloat-abi=softfp \
-    -mfpu=neon
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
